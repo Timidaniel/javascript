33ba87ed719c68dfaf45683ba2da6cb533990dd7
oladipupodaniel207@gmail.com <oladipupodaniel207@gmail.com>
2024-08-22 06:20:40 -0700

    first commit

---
+++ assets/js/learn.js
@@ -0,0 +1,9 @@
+alert('hello you are welcome')
+let person={
+    id: 2,
+    firstname: "jacob",
+    lastname: "adeleke",
+    age: 12,
+    location:"kenya",
+}
+     console.log(" My name is " + firstname + lastname + " and i am " + age + " years old i live in + location");
